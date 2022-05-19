adf9b3a0338e1d0a282b86681ae6a390d1ed7830
BILAL AHMAD SOFI <42963374+sofibilal193@users.noreply.github.com>
2022-05-19 22:51:47 +0530

    Add files via upload

---
+++ Index.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="StripePaymentCheckout.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <button type="submit">Checkout</button>
+
+    </form>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script>
+        // Stripe Publishable Key  
+        var stripe = Stripe('pk_test_51KxsOcSCpeElc4NAgMwjZGuNcKM3r3oS5kQBX8B31IwfjmoVSgKwxVdDWwET0LQHGzgt6OzqZvwtjVFSWCnEN8eU00njbgTyLz');
+        var form = document.getElementById("form1");
+
+        // Onclick 
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            // Redirects to Stripe Checkout Sessioon
+            stripe.redirectToCheckout({
+                sessionId: "<%= sessionId %>"
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,49 @@
+﻿using Stripe;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StripePaymentCheckout
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        public string sessionId = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Create Checkout Session
+
+            // Secret Key--Stripe
+            StripeConfiguration.ApiKey = "sk_test_51KxsOcSCpeElc4NAFr9lUqswE0BtyqSwpj143KTiwBigpN0jeM2d6471JCv7yDiRAIgFx8NqCa2rObQ8wPqNOxaS00DiT1sbE7";
+
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = "https://localhost:44325/success?id={CHECKOUT_SESSION_ID}",
+                CancelUrl = "https://localhost:44325/cancel",
+                PaymentMethodTypes = new List<string>
+                {
+                    "card",
+                },
+                LineItems = new List<SessionLineItemOptions>
+              {
+                new SessionLineItemOptions
+                {
+                 Name = "T-Shirt",
+                 Description = "Cotton Made Comfortable",
+                 Amount = 1000,
+                 Currency = "usd",
+                 Quantity = 2,
+                },
+              },
+                Mode = "payment",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            sessionId = session.Id;
+        }
+    }
+}
